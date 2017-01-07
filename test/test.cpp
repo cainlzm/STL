7406197d3de0b653394334e9d82a13acfbbad417
cainlzm <coutzm163@163.com>
2017-01-07 16:08:46 +0800

    update

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,13 @@
 #include<iostream>
 #include "stringTest.h"
+#include "zm_stl_construct.h"
 int main()
 { 
-	stringTest();
+	int a=0;
+	int b = 2;
+	std::cout << a << std::endl;
+	zm::construct(&a,b);
+	std::cout << a << std::endl;
+	//stringTest();
 	return 0;
 }
